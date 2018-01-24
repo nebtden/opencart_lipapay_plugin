2310b4af0caa56ee02aedb28cea920b573e2fa9b
simon.zhang <simon.zhang@kilimall.com>
2018-01-24 18:31:41 +0800

    添加商品名

--- catalog/controller/extension/payment/lipapay.php
+++ catalog/controller/extension/payment/lipapay.php
@@ -8,15 +8,17 @@ class ControllerExtensionPaymentLipapay extends Controller {
         $order_info = $this->model_checkout_order->getOrder($this->session->data['order_id']);
         $out_trade_no = trim($order_info['order_id']);
         $total_amount = trim($this->currency->format($order_info['total'], $this->config->get('payment_lipapay_monetary_unit'), '', false));
+
+        $ordered_products = $this->model_checkout_order->getOrderProducts($this->session->data['order_id']);
         $config = array (
             'merchantId'          => $this->config->get('payment_lipapay_app_id'),
-            'merchantKey'         => $this->config->get('payment_lipapay_app_id'),
-            'notifyUrl'           => HTTP_SERVER . "payment_callback/lipapay",
+            'merchantKey'         => $this->config->get('payment_lipapay_merchant_private_key'),
+            'notifyUrl'           => HTTP_SERVER . 'index.php?route=extension/payment/lipapay/notify',
             'returnUrl'           => $this->url->link('checkout/success'),
 
             'signType'            => "MD5",
-//            'goodsName'            => $order_info['goods'][0],
-            'goodsName'           => 'test',
+            'goodsName'            => $ordered_products[0]['name'],
+//            'goodsName'           => 'test',
             'goodsType'           => "2",
             'expirationTime'      => "10000",
             'sourceType'          => "B",
@@ -44,38 +46,37 @@ class ControllerExtensionPaymentLipapay extends Controller {
         return $this->load->view('extension/payment/lipapay', $config);
     }
 
-    public function callback() {
+    public function notify() {
         $this->log->write('LipaPay pay notify:');
         $arr = $_POST;
-        $config = array (
-            'app_id'               => $this->config->get('payment_lipapay_app_id'),
-            'merchant_private_key' => $this->config->get('payment_lipapay_merchant_private_key'),
-            'notify_url'           => HTTP_SERVER . "payment_callback/lipapay",
-            'return_url'           => $this->url->link('checkout/success', 'language=' . $this->config->get('config_language')),
-            'charset'              => "UTF-8",
-            'sign_type'            => "MD%",
-            'gateway_url'          => $this->config->get('payment_lipapay_test') == "sandbox" ? "http://sandbox.lipapay.com/api/excashier.html" : "https://www.lipapay.com/api/excashier.html",
-            'LipaPay_public_key'    => $this->config->get('payment_lipapay_lipapay_public_key'),
-        );
-        $this->load->model('extension/payment/LipaPay');
+        $sign = $arr['sign'];
+        unset($arr['sign']);
+        $this->load->model('extension/payment/lipapay');
         $this->log->write('POST' . var_export($_POST,true));
-        $result = $this->model_extension_payment_lipapay->check($arr, $config);
+        $result = $this->model_extension_payment_lipapay->generateSign($arr, $this->config->get('payment_lipapay_merchant_private_key'));
 
-        if($result) {//check successed
-            $this->log->write('LipaPay check successed');
-            $order_id = $_POST['out_trade_no'];
-            if($_POST['trade_status'] == 'TRADE_FINISHED') {
+        if($result==$sign) {//check successed
+            $this->log->write('lipapay check successed');
+            $order_id = $arr['merchantOrderNo'];
+            if($_POST['status'] == 'SUCCESS') {
+                $return = [];
+                $return['status'] = 'SUCCESS';
+                $return['errorCode'] = '100';
+                $return['merchantId'] = $arr['merchantId'];
+                $return['signType'] = 'MD5';
+                $return['merchantOrderNo'] = $arr['merchantOrderNo'];
+                $return['orderId'] = $arr['orderId'];
+
+                $mysign = $this->model_extension_payment_lipapay->generateSign($return, $this->config->get('payment_lipapay_merchant_private_key'));
+                $return['sign'] =$mysign;
+
+                echo json_encode($return);
             }
-            else if ($_POST['trade_status'] == 'TRADE_SUCCESS') {
-                $this->load->model('checkout/order');
-                $this->model_checkout_order->addOrderHistory($order_id, $this->config->get('payment_lipapay_order_status_id'));
-            }
-            echo "success";	//Do not modified or deleted
+
+
         }else {
-            $this->log->write('LipaPay check failed');
-            //chedk failed
+            $this->log->write('lipapay check failed');
             echo "fail";
-
         }
     }
 }
